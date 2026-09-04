6ac70435f13e89e38b16df0a0a349dfc0ae169c3
Shatha Alharbi <shaxtha@gmail.com>
2026-09-05 00:31:39 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * GccApplication2.cpp
+ *
+ * Created: 02/05/2026 10:59:45 PM
+ * Author : ashat
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
